bd46531c74cc2875587c6d49747d0a1a119186eb
umaidashi <umaidashi18@gmail.com>
2024-12-31 00:40:30 +0900

    feat: entrypoint

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,30 @@
 
 import { cli } from "cleye";
 import { version } from "../package.json";
-import { checkCommandIsInstalled } from "./utils";
+import { checkCommandIsInstalled, checkGhCopilotIsInstalled } from "./utils";
+import { template } from "./template";
+import { ghcmdExplain } from "./ghcmd";
 
 export const CLI = cli(
   {
     name: "ghcommit",
     version,
+    //flags: {
+    //  template: String,
+    //},
   },
   async () => {
     const isInstalledGh = await checkCommandIsInstalled("gh");
     if (!isInstalledGh) {
       console.error("gh is not installed.");
     }
-
-    const isInstalledGhCopilot = await checkCommandIsInstalled("gh copilot");
+    const isInstalledGhCopilot = await checkGhCopilotIsInstalled();
     if (!isInstalledGhCopilot) {
       console.error("gh copilot extension is not installed.");
     }
 
-    //await $`gh`;
+    const test = await ghcmdExplain();
+
+    console.log(JSON.stringify(test, null, " "));
   },
 );
