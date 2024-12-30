89dd7b666559fdf1cbb0849415d217517492b94c
umaidashi <umaidashi18@gmail.com>
2024-12-31 00:40:07 +0900

    feat: add util

--- src/utils.ts
+++ src/utils.ts
@@ -6,3 +6,9 @@ export const checkCommandIsInstalled = async (
   const { exitCode } = await $`${command}`.nothrow().quiet();
   return exitCode === 0;
 };
+
+export const checkGhCopilotIsInstalled = async (): Promise<boolean> => {
+  const list = await $`gh extension list`.text();
+
+  return list.includes("github/gh-copilot");
+};
