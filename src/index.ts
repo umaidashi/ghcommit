58105f2e68e2eea2b4eccff8940d70d0f11cae0f
umaidashi <umaidashi18@gmail.com>
2024-12-30 18:46:05 +0900

    feat: check install commands

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello via Bun!");
+#!/usr/bin/env bun
+
+import { cli } from "cleye";
+import { version } from "../package.json";
+import { checkCommandIsInstalled } from "./utils";
+
+export const CLI = cli(
+  {
+    name: "ghcommit",
+    version,
+  },
+  async () => {
+    const isInstalledGh = await checkCommandIsInstalled("gh");
+    if (!isInstalledGh) {
+      console.error("gh is not installed.");
+    }
+
+    const isInstalledGhCopilot = await checkCommandIsInstalled("gh copilot");
+    if (!isInstalledGhCopilot) {
+      console.error("gh copilot extension is not installed.");
+    }
+
+    //await $`gh`;
+  },
+);
